72c88f1d93a5a9f7d1b0856b171f20110a0230f4
stifamajstor <stjepan.crncic@gmail.com>
2020-03-17 21:24:12 +0100

    deploying

---
+++ build/precache-manifest.b3ca232562cff23eedf9d4a987b64239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8997cfb0a29a8f60df211d4f068ca73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e932768cdb8c2f9695fc",
+    "url": "/static/css/main.2af03e5e.chunk.css"
+  },
+  {
+    "revision": "53c440018a1054909db5",
+    "url": "/static/js/2.b0af9526.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b0af9526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e932768cdb8c2f9695fc",
+    "url": "/static/js/main.fb8e320f.chunk.js"
+  },
+  {
+    "revision": "29a7d1f659258c9ae662",
+    "url": "/static/js/runtime-main.8ad0d90e.js"
+  }
+]);
